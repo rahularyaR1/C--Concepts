45f89ea7d5be083611d98f849f2512cb86f52127
Rahul Arya <rahularya2310@gmail.com>
2022-11-12 17:34:43 +0530

    Master

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -1,18 +1,12 @@
-﻿using AccessModifiersLib;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Program 
 {
-    public class AccessModifiers
+    public class CSharp_Concepts
     {
         public static void Main(string[] args)
         {
-            string am = "Public, Private, Protected, Private Protected, Protected Internal, Internal";
-            Console.WriteLine($"Access Modifiers in C# specifies who can see and use the resources.\nThere are 6 different types of Access Modifiers : {am}.");
-            AccessModifiersLibrary aml = new();
-            aml.Public("");
-            CallingDemo callingDemo = new();
-            callingDemo.MakeDemoCalls();
+            
         }
     }
 }
